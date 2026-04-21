5429b66f3c09b36cf6614f5ad312c7274b2206b1
Alan-Rico <oscaralan1806@gmail.com>
2026-04-21 14:33:06 -0600

    Repositorios CRUD sin escribir SQL

---
+++ src/main/java/com/alan/play/persistence/crud/CrudMovieEntity.java
@@ -0,0 +1,8 @@
+package com.alan.play.persistence.crud;
+
+import com.alan.play.persistence.entity.MovieEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
+
+}
